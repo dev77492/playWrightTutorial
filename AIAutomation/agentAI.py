ebcc4e28de73db167a817896fae0fa0067f06a22
dev77492 <aniketsawant94053@gmail.com>
2025-02-18 23:19:29 +0530

    Added AI agent to the playwright library

---
+++ AIAutomation/agentAI.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+
+from browser_use.agent.service import Agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pydantic import SecretStr
+
+
+async def siteValidation():
+    os.environ["GEMINI_API_KEY"]= "AIzaSyCOYEfVtWdNRYtzjYhXueyOdmVno0IKBNQ"
+    task =(
+        'Important: I am UI Automation tester validating the tasks'
+        'Open website https://rahulshettyacademy.com/loginpagePractise/'
+        'Login with username and password. login Details available in the same page'
+        'After login, select first 2 products and add them to cart'
+        'Then checkout and store the total value you see in screen'
+        'Increase the quantity of any product and check if total value update accordingly'
+        'checkout and select country, agree terms and purchase'
+        'verify thankyou message is displayed'
+    )
+
+    api_key = os.environ["GEMINI_API_KEY"]
+
+    llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(api_key))
+    agent = Agent(task,llm,use_vision=True)
+    history = await agent.run()
+    test_result=history.final_result()
+    print(test_result)
+
+
+
+asyncio.run(siteValidation())
